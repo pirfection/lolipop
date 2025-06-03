939d430678e133373fb37ffee4152bc99feb1ad1
pirfection <artemiy_2020@internet.ru>
2025-06-03 17:57:13 +0300

    lolka

---
+++ lolka.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print('Привет')
+    for j in range(5):
+        print('Привет')
+        for k in range(5):
+            print('Привет')
+            for l in range(5):
+                print('Привет')
